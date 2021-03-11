9eeb41f0aaf32a78fa55b421c05eaace9ea662c1
Marvin Kastner <marvin.kastner@tuhh.de>
2021-03-11 17:35:55 +0100

    Verbessere start-Script

--- start.bat
+++ start.bat
@@ -1,5 +1,9 @@
 set ANACONDA_ROOT=C:\Users\%USERNAME%\Anaconda3
+IF NOT EXIST %ANACONDA_ROOT% PAUSE
 
-set PATH=%ANACONDA_ROOT%\envs\intro-jupyter-notebooks\Scripts;%ANACONDA_ROOT%\envs\intro-jupyter-notebooks;%PATH%;%ANACONDA_ROOT%\Scripts
+set ENV_SPECIFIC_ROOT=%ANACONDA_ROOT%\envs\intro-jupyter-notebooks
+IF NOT EXIST %ENV_SPECIFIC_ROOT% PAUSE
+
+set PATH=%ENV_SPECIFIC_ROOT%\Scripts;%ENV_SPECIFIC_ROOT%;%ANACONDA_ROOT%\Scripts;%ANACONDA_ROOT%;%PATH%
 
 call activate intro-jupyter-notebooks && jupyter lab || pause
