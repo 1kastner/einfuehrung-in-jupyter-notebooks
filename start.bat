ac73e4d18631995523d9eaae776503189f9074bb
Marvin Kastner <marvin.kastner@tuhh.de>
2021-03-11 17:57:19 +0100

    Vereinfache Startskript

--- start.bat
+++ start.bat
@@ -1,9 +1,4 @@
 set ANACONDA_ROOT=C:\Users\%USERNAME%\Anaconda3
 IF NOT EXIST %ANACONDA_ROOT% PAUSE
 
-set ENV_SPECIFIC_ROOT=%ANACONDA_ROOT%\envs\intro-jupyter-notebooks
-IF NOT EXIST %ENV_SPECIFIC_ROOT% PAUSE
-
-set PATH=%ENV_SPECIFIC_ROOT%\Scripts;%ENV_SPECIFIC_ROOT%;%ANACONDA_ROOT%\Scripts;%ANACONDA_ROOT%;%PATH%
-
-call activate intro-jupyter-notebooks && jupyter lab || pause
+call %ANACONDA_ROOT%\Scripts\activate intro-jupyter-notebooks && jupyter lab || pause
